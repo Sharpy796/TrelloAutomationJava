c805931ab82f0dde55a6f1f27d564f9fd43b0787
Christian <christian.bankovic@gmail.com>
2025-11-11 13:45:40 -0600

    create checkbox dropdown

--- app/src/main/java/com/example/trelloautomationjava/MainActivity.java
+++ app/src/main/java/com/example/trelloautomationjava/MainActivity.java
@@ -4,15 +4,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.DialogInterface;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import org.json.JSONException;
 
 import java.io.IOException;
@@ -34,6 +39,15 @@ public class MainActivity extends AppCompatActivity {
     public final int[] HOUR_ARR_INT = {1,2,3,4,5,6,7,8,9,10,11,12};
     public final int[] MIN_ARR_INT = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59};
 
+    ///
+
+    TextView textView;
+    boolean[] selectedLanguage;
+    ArrayList<Integer> langList = new ArrayList<>();
+    String[] langArray = {"Java", "C++", "Kotlin", "C", "Python", "Javascript"};
+
+    ///
+
     public MainActivity() throws JSONException, IOException {
     }
 
@@ -47,6 +61,95 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ///////////
+
+        // assign variable
+        textView = findViewById(R.id.textView);
+
+        // initialize selected language array
+        selectedLanguage = new boolean[langArray.length];
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Initialize alert dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                // set title
+                builder.setTitle("Select Language");
+
+                // set dialog non cancelable
+                builder.setCancelable(false);
+
+                builder.setMultiChoiceItems(langArray, selectedLanguage, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                        // check condition
+                        if (b) {
+                            // when checkbox selected
+                            // Add position  in lang list
+                            langList.add(i);
+                            // Sort array list
+                            Collections.sort(langList);
+                        } else {
+                            // when checkbox unselected
+                            // Remove position from langList
+                            langList.remove(Integer.valueOf(i));
+                        }
+                    }
+                });
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Initialize string builder
+                        StringBuilder stringBuilder = new StringBuilder();
+                        // use for loop
+                        for (int j = 0; j < langList.size(); j++) {
+                            // concat array value
+                            stringBuilder.append(langArray[langList.get(j)]);
+                            // check condition
+                            if (j != langList.size() - 1) {
+                                // When j value  not equal
+                                // to lang list size - 1
+                                // add comma
+                                stringBuilder.append(", ");
+                            }
+                        }
+                        // set text on textView
+                        textView.setText(stringBuilder.toString());
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // dismiss dialog
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.setNeutralButton("Clear All", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // use for loop
+                        for (int j = 0; j < selectedLanguage.length; j++) {
+                            // remove all selection
+                            selectedLanguage[j] = false;
+                            // clear language list
+                            langList.clear();
+                            // clear text view value
+                            textView.setText("");
+                        }
+                    }
+                });
+                // show dialog
+                builder.show();
+            }
+        });
+
+        /// ///////
 
         giveCreateCardButtonFunctionality();
 //        changeTextOnce();
@@ -65,7 +168,13 @@ public class MainActivity extends AppCompatActivity {
         createCardButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cardCreator.createTrelloCard("test", "Monday", "test", new String[]{"TKE", "KoC"});
+                String name = getTextFromBox(R.id.name);
+                String desc = getTextFromBox(R.id.description);
+                String list = getTextFromSpinner(R.id.lists);
+                String labels = getTextFromSpinner(R.id.labels);
+                if (!name.isEmpty() && !list.isEmpty()) {
+                    cardCreator.createTrelloCard(name, list, desc, new String[]{labels});
+                }
 //                int random = (int) (Math.random() * manyDifferentStrings.length);
 //                if (random == oldValue) {
 //                    random = (int) (Math.random() * manyDifferentStrings.length);
@@ -86,6 +195,16 @@ public class MainActivity extends AppCompatActivity {
                 changingText.setText("Hello");
             }
         });
+    }
+
+    private String getTextFromBox(int id) {
+        EditText text = (EditText)findViewById(id);
+        return text.getText().toString();
+    }
+
+    private String getTextFromSpinner(int id) {
+        Spinner spinner_house = (Spinner) findViewById(id);
+        return spinner_house.getSelectedItem().toString();
     }
 
     private void setUpDropdown(int id, String[] list) {
