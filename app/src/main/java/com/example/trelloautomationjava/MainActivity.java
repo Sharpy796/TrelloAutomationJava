a55cd0b6a098f58b6f69e335960b86934309fc79
Christian <christian.bankovic@gmail.com>
2026-02-12 15:57:29 -0600

    new timepicker working

--- app/src/main/java/com/example/trelloautomationjava/MainActivity.java
+++ app/src/main/java/com/example/trelloautomationjava/MainActivity.java
@@ -80,7 +80,7 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
     public final int[] MIN_ARR_INT = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59};
     final Handler CreateCardRunnableHandler = new Handler();
     final LocalDateTime todayDate = getToday();
-    int[] dueDate = new int[5];
+    int[] dueDate = new int[6];
     int checkedListItem = 0;
 
     MyRecyclerViewAdapter adapter;
@@ -94,7 +94,7 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
     protected void onCreate(Bundle savedInstanceState) {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            updateDueDateValue(todayDate.getYear(), todayDate.getMonthValue(), todayDate.getDayOfMonth(), 19, 0);
+            updateDueDateValue(todayDate.getYear(), todayDate.getMonthValue(), todayDate.getDayOfMonth(), 19, 0, 1);
         }
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -134,18 +134,30 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
         DatePicker dp = (DatePicker) linlayout.findViewById(R.id.datepicker);
         FlexibleNumberPicker hp = (FlexibleNumberPicker) linlayout.findViewById(R.id.hours);
         FlexibleNumberPicker mp = (FlexibleNumberPicker) linlayout.findViewById(R.id.mins);
-        setUpListPicker(hp, HOUR_ARR_STR, 6, Gravity.CENTER_HORIZONTAL);
-        setUpListPicker(mp, MIN_ARR_STR, 0, Gravity.CENTER_HORIZONTAL);
-
+        setUpListPicker(hp, HOUR_ARR_STR, dueDate[3]-(dueDate[3] > 12 ? 13 : 1), Gravity.CENTER_HORIZONTAL);
+        setUpListPicker(mp, MIN_ARR_STR, dueDate[4]/5, Gravity.CENTER_HORIZONTAL);
         Button buttonAM = linlayout.findViewById(R.id.button_am);
         Button buttonPM = linlayout.findViewById(R.id.button_pm);
+
+        int[] ampm = new int[1];
+        ampm[0] = dueDate[5];
+
+        if (ampm[0] == 0) {
+            buttonAM.setBackgroundTintList(getResources().getColorStateList(R.color.button_selected, null));
+            buttonAM.setTextColor(getResources().getColorStateList(R.color.text_success, null));
+            buttonPM.setBackgroundTintList(getResources().getColorStateList(R.color.button_unselected, null));
+            buttonPM.setTextColor(getResources().getColorStateList(R.color.text_danger, null));
+        }
+
         buttonAM.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(LOG_TAG, "am pressed");
                 buttonAM.setBackgroundTintList(getResources().getColorStateList(R.color.button_selected, null));
+                buttonAM.setTextColor(getResources().getColorStateList(R.color.text_success, null));
                 buttonPM.setBackgroundTintList(getResources().getColorStateList(R.color.button_unselected, null));
-
+                buttonPM.setTextColor(getResources().getColorStateList(R.color.text_danger, null));
+                ampm[0] = 0;
             }
         });
         buttonPM.setOnClickListener(new OnClickListener() {
@@ -153,10 +165,12 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
             public void onClick(View v) {
                 Log.d(LOG_TAG, "pm pressed");
                 buttonPM.setBackgroundTintList(getResources().getColorStateList(R.color.button_selected, null));
+                buttonPM.setTextColor(getResources().getColorStateList(R.color.text_success, null));
                 buttonAM.setBackgroundTintList(getResources().getColorStateList(R.color.button_unselected, null));
+                buttonAM.setTextColor(getResources().getColorStateList(R.color.text_danger, null));
+                ampm[0] = 1;
             }
         });
-//            buttonAM.setBackground(ContextCompat.getDrawable(this, R.drawable.button_rectangle_green));
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Pick Due Date");
@@ -167,10 +181,15 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
                 int year = dp.getYear();
                 int monthOfYear = dp.getMonth()+1;
                 int dayOfMonth = dp.getDayOfMonth();
-//                int hourOfDay = tp.getHour();
-//                int minute = tp.getMinute();
-//                updateDueDateText(year, monthOfYear, dayOfMonth, hourOfDay, minute);
-//                updateDueDateValue(year, monthOfYear, dayOfMonth, hourOfDay, minute);
+                int hourOfDay = HOUR_ARR_INT[hp.getValue()];
+                if (hourOfDay == 12) {
+                    if (ampm[0] == 0) {hourOfDay -= 12;}
+                } else {hourOfDay += 12*(ampm[0]);}
+                Log.w(LOG_TAG, "hour:"+hourOfDay);
+                Log.w(LOG_TAG, "ampm:"+ampm[0]);
+                int minute = MIN_ARR_INT[mp.getValue()];
+                updateDueDateText(year, monthOfYear, dayOfMonth, hourOfDay, minute);
+                updateDueDateValue(year, monthOfYear, dayOfMonth, hourOfDay, minute, ampm[0]);
             }
         });
 
@@ -192,66 +211,67 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
 
     }
 
-    private void openDatePicker() {
-        LocalDateTime today = getToday();
-        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                openTimePicker(year, month+1, dayOfMonth);
-            }
-        };
-        DatePickerDialog datePickerDialog = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            datePickerDialog = new DatePickerDialog(this,
-    //                android.R.style.Theme,
-                    dateSetListener, dueDate[0], dueDate[1]-1, dueDate[2]);
-        } else {
-            Log.w(LOG_TAG, "Why do i have to do this fricken thing aaaaaaaaa.");
-        }
-
-        datePickerDialog.show();
-    }
-    private void openTimePicker(int year, int monthOfYear, int dayOfMonth) {
-        TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                updateDueDateText(year, monthOfYear, dayOfMonth, hourOfDay, minute);
-                updateDueDateValue(year, monthOfYear, dayOfMonth, hourOfDay, minute);
-            }
-        };
-        // TODO: Fix display of this so the ok button is in the same spot as the datepicker's
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
-                android.R.style.Theme_Holo_Dialog_NoActionBar,
-                timeSetListener, dueDate[3], dueDate[4], false);
-        timePickerDialog.show();
-    }
+//    private void openDatePicker() {
+//        LocalDateTime today = getToday();
+//        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                openTimePicker(year, month+1, dayOfMonth);
+//            }
+//        };
+//        DatePickerDialog datePickerDialog = null;
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            datePickerDialog = new DatePickerDialog(this,
+//    //                android.R.style.Theme,
+//                    dateSetListener, dueDate[0], dueDate[1]-1, dueDate[2]);
+//        } else {
+//            Log.w(LOG_TAG, "Why do i have to do this fricken thing aaaaaaaaa.");
+//        }
+//
+//        datePickerDialog.show();
+//    }
+//    private void openTimePicker(int year, int monthOfYear, int dayOfMonth) {
+//        TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+//            @Override
+//            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//                updateDueDateText(year, monthOfYear, dayOfMonth, hourOfDay, minute);
+//                updateDueDateValue(year, monthOfYear, dayOfMonth, hourOfDay, minute);
+//            }
+//        };
+//        // TODO: Fix display of this so the ok button is in the same spot as the datepicker's
+//        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+//                android.R.style.Theme_Holo_Dialog_NoActionBar,
+//                timeSetListener, dueDate[3], dueDate[4], false);
+//        timePickerDialog.show();
+//    }
 
     private void updateDueDateText() {
         updateDueDateText(dueDate[0],dueDate[1],dueDate[2],dueDate[3],dueDate[4]);
     }
 
-    private void updateDueDateValue(int year, int monthOfYear, int dayOfMonth, int hourOfDay, int minute) {
+    private void updateDueDateValue(int year, int monthOfYear, int dayOfMonth, int hourOfDay, int minute, int ampm) {
         dueDate[0] = year;
         dueDate[1] = monthOfYear;
         dueDate[2] = dayOfMonth;
         dueDate[3] = hourOfDay;
         dueDate[4] = minute;
+        dueDate[5] = ampm; // 0 = AM, 1 = PM
     }
 
     private String parseDueDate() {
         // 2025-11-14T19:00:00-06:00
         String message = "";
-        message += dueDate[0]+"-";
-        if (dueDate[1] < 10) {
+        message += dueDate[0]+"-"; // year
+        if (dueDate[1] < 10) { // month
             message += "0";
         } message += dueDate[1]+"-";
-        if (dueDate[2] < 10) {
+        if (dueDate[2] < 10) { // day
             message += "0";
         } message += dueDate[2]+"T";
-        if (dueDate[3] < 10) {
+        if (dueDate[3] < 10) { // hour
             message += "0";
         } message += dueDate[3]+":";
-        if (dueDate[4] < 10) {
+        if (dueDate[4] < 10) { // minute
             message += "0";
         } message += dueDate[4]+":00-06:00";
         Log.i(LOG_TAG, "Date:\t"+message);
